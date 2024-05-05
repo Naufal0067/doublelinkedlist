ac82a8a9656e6521cb38d09d99fe042c54395366
Naufal0067 <naufalopang1928@gmail.com>
2024-05-05 08:46:25 +0700

    menambahkan new node

--- 0067_Doublelinkedlist.cpp
+++ 0067_Doublelinkedlist.cpp
@@ -28,6 +28,8 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl; 
             return;
         }
+        newNode->next = START;
+       if (START != NULL)
     }
     
 }
